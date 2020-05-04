804dfe17ea364bfa41f702a09a32923dd52ccc8b
Beedeeboom <stephanie.duffieux@gmail.com>
2020-05-04 20:56:52 +1000

    added BooksGenre model and form_with for new Books

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'books/new'
+  post 'books', to: 'books#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
