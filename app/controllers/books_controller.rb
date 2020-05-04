a8f2682ca2c87861df6b30b92316a867332319b1
Beedeeboom <stephanie.duffieux@gmail.com>
2020-05-04 22:29:55 +1000

    Added form validations

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,23 @@
 class BooksController < ApplicationController
   def new
+    @authors = Author.all
+    @genres = Genre.all
     @book = Book.new
   end
   def create
+    @authors = Author.all
+    @genres = Genre.all
+    @book = Book.new(book_params)
+    if @book.save
+      render json: @book
+    else
+      flash[:alert] = @book.errors.full_messages.join()
+      render "new"
+    end
+  end
 
+  private
+  def book_params
+    params.require(:book).permit(:title, :author_id, :genre_ids => [])
   end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,6 @@ class Book < ApplicationRecord
   belongs_to :author
   has_many :books_genres
   has_many :genres, through: :books_genres
+
+  validates :title, presence: true, length: { minimum: 3 }
 end
