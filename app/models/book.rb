5d410d7acd96b9212a79869299be7bfb24c7f32b
Beedeeboom <stephanie.duffieux@gmail.com>
2020-05-04 23:22:28 +1000

    added book list model and controller

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,8 @@ class Book < ApplicationRecord
   belongs_to :author
   has_many :books_genres
   has_many :genres, through: :books_genres
+  has_many :book_lists
+  has_many :users, through: :book_lists
 
   validates :title, presence: true, length: { minimum: 3 }
 end
